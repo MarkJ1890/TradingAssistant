5d0df36e4ff7b11a089acd1d819a0d25501ce854
MarkJ1890 <jilderdamark@gmail.com>
2025-04-30 09:05:19 +0200

    Update data.py

--- data.py
+++ data.py
@@ -8,11 +8,3 @@ def get_data(ticker: str, period='5d', interval='1h'):
     except Exception as e:
         print(f"Fout bij ophalen van data: {e}")
         return pd.DataFrame()
-
-def get_data_5m(ticker: str):
-    try:
-        df = yf.download(ticker, period='1d', interval='5m')
-        return df
-    except Exception as e:
-        print(f"Fout bij ophalen van 5m data: {e}")
-        return pd.DataFrame()
